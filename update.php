a3ba5ed81ec081bd0792d89c851993850f0d0c2b
devilghostg <mister.devil6@gmail.com>
2024-10-25 16:12:53 +0400

    presque fini

--- update.php
+++ update.php
@@ -10,15 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $selected_player = $_POST['selected_player'];
     $new_nom = $_POST['new_nom'];
 
-    // Vérifiez si le nouveau nom n'est pas vide et ne contient pas d'espaces
     if (!empty($new_nom) && !preg_match('/\s/', $new_nom)) {
-        // Mettre à jour le nom dans la base de données
         $stmt = $conn->prepare("UPDATE joueurs SET nom = :new_nom WHERE nom = :selected_player");
         $stmt->bindParam(':new_nom', $new_nom);
         $stmt->bindParam(':selected_player', $selected_player);
 
         if ($stmt->execute()) {
-            // Redirigez vers la page d'accueil avec un message de succès
             header('Location: index.php?message=Pseudo modifié avec succès.');
             exit;
         } else {
@@ -26,7 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "Erreur lors de la mise à jour du pseudo.";
         }
     } else {
-        // Gérer l'erreur de validation
         echo "Le pseudo ne doit pas être vide ou contenir des espaces.";
     }
 }
